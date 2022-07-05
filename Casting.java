6f9d86fc14bae3c71532ccafe849d00394693862
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-05 11:35:42 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,11 @@
+class Casting
+{
+public static void main(String arg[])
+{
+char letter='A';
+int ascii=letter;
+char alp=(char)ascii;
+System.out.println("value of int value of letter:"+ascii);
+System.out.println("value of char value of letter:"+alp);
+}
+}
