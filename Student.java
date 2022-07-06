e8fd19f55e85aa6f593fb4caef9faef726989269
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-06 13:04:25 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Student
+{
+	public static void main(String arg[])
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the Student ID:");
+		byte StudentId=s.nextByte();
+		System.out.println("Enter the Student Name:");
+		String  StudentName=s.next();
+		System.out.println("Enter the Student Gender(M/F):");
+		char StudentGender=s.next().charAt(0);
+		System.out.println("Enter the Student Age:");
+		byte StudentAge=s.nextByte();
+		System.out.println("Enter the Student mobile NO.:");
+		long StudentMObNo=s.nextLong();
+		System.out.println("Enter the Student CGPA:");
+		float StudentCGPA=s.nextfloat();
+		
+		
+		
+		System.out.println("Enter the Student ID: "+StudentId);
+		System.out.println("Enter the Student Name: "+StudentName);
+		System.out.println("Enter the Student Gender: "+StudentGender);
+		System.out.println("Enter the Student Age: "+StudentAge);
+		System.out.println("Enter the Student mobile number: "+StudentMObNo);
+		System.out.println("Enter the Student CGPA: "+StudentCGPA);
+	}
+}
+		
+		
+		
+		
+		
+		
