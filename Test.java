23bc0f614bc8c637e4ae24082b858b7b43b1fb87
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-07 11:52:28 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package mine;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		BeanExample B1= new BeanExample();
+		B1.setId(20);
+		System.out.println("Id value :-"+B1.getId());
+		B1.setName("abcb");
+		System.out.println("name value:- "+B1.getName());
+
+	}
+
+}
