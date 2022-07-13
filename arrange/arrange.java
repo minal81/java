abf027b9af8c24688593c6336b294bffa7aa8577
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-13 12:51:56 +0530

    Create arrange.java

---
+++ arrange/arrange.java
@@ -0,0 +1,16 @@
+class arrange
+{
+public static void main(String args[])
+        {
+          System.out.println("Asending order");
+                for(int i=1;i<=10;i++)
+                {
+                  System.out.println(i);
+                }
+          System.out.println("Desending order");
+                for(int j=10;j>=1;j--)
+                {
+                  System.out.println(j);
+                }
+          }
+}
