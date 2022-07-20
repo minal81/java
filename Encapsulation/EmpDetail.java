dde8c662ccf7ae451a713749bb1a9746b799cff5
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-20 11:56:38 +0530

    Create EmpDetail.java

---
+++ Encapsulation/EmpDetail.java
@@ -0,0 +1,18 @@
+
+public class EmpDetail 
+{
+
+	public static void main(String[] args) 
+	{
+		Emp E1=new Emp();
+		E1.setEmpName("Ram");
+		E1.setEmpAge(25);
+		E1.setEmpAaddhar(487651234);
+		System.out.println("Emp Name:"+E1.getEmpName());
+		System.out.println("Emp Name:"+E1.getEmpAge());
+		System.out.println("Emp Name:"+E1.getEmpAaddhar());
+		
+
+	}
+
+}
