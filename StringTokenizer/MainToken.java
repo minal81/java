05674e3fe168a2acf2bb072f5a461e698cc31280
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-07-27 12:43:33 +0530

    Create MainToken.java

---
+++ StringTokenizer/MainToken.java
@@ -0,0 +1,38 @@
+import java.util.StringTokenizer;
+class StringT
+{
+	void display()
+	{
+		StringTokenizer st1=new StringTokenizer("You are a woman"," ");
+		System.out.println("token of StringT class:");
+		while(st1.hasMoreTokens())
+		{
+			System.out.println(st1.nextToken());
+		}
+	}
+}
+class StringY
+{
+	void display1()
+	{
+		StringTokenizer st2=new StringTokenizer("Tomorrow is Monday"," ");
+		System.out.println("token of StringY class:");
+		while(st2.hasMoreTokens())
+		{
+			System.out.println(st2.nextToken());
+		}
+	}
+}
+class MainToken
+{
+	public static void main(String args[])
+	{
+		StringT s1=new StringT();
+		StringY s2=new StringY();
+		s1.display();
+		s2.display1();
+	}
+}
+		
+	
+	
