0c617c7008aea5f458cd975fb640a303a23f3465
Minal Gavhane <80057100+minal81@users.noreply.github.com>
2022-08-02 12:16:40 +0530

    Update Desending.java

--- Searching & Sorting/Desending.java
+++ Searching & Sorting/Desending.java
@@ -4,20 +4,15 @@ class Desending
 {
 	public static void main(String ARG[])
 	{
-      Integer[] Arr=new Integer[]{10,7,9,13,17};
-      Character[] Arr1=new Character[]{'e','h','g','a','c'};
-      Arrays.sort(Arr,Collections.reverseOrder());
-      System.out.println("integer sorted array");
-      for(int i=0;i<5;i++)
-      {
-          System.out.println(""+Arr[i]);
-      }
-      Arrays.sort(Arr1,Collections.reverseOrder());
-      System.out.println("charater sorted array");
-      for(int i=0;i<5;i++)
-      {
-          System.out.println(""+Arr1[i]);
-      }
+	      Integer[] Arr=new Integer[]{10,7,9,13,17};
+	      Character[] Arr1=new Character[]{'e','h','g','a','c'};
+	      Arrays.sort(Arr,Collections.reverseOrder());
+	      System.out.println("integer sorted array");
+	     System.out.println(""+Arrays.toString(Arr));
+
+	      Arrays.sort(Arr1,Collections.reverseOrder());
+	      System.out.println("charater sorted array");
+	      System.out.println(""+Arrays.toString(Arr1));
 	}
 }
 	
